b18e606f473ca713c8b4a7a6a20db5d07825e14d
jeanettestock <jeanettemstock@gmail.com>
2020-05-07 21:45:48 +0000

    In Class Defining and connecting routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,52 @@
 class ApplicationController < ActionController::Base
-  layout(false)
-
   # Add your actions below this line
   # ================================
 
-end
+    def homepage
+      render({ :plain => "Hello, world!" })
+    end
+    
+    def play_rock
+      moves = ["rock", "paper", "scissors"]
+      computer_move = moves.sample
+      
+      if computer_move == "rock"
+        outcome = "tied"
+      elsif computer_move == "paper"
+        outcome = "lost"
+      elsif computer_move == "scissors"
+        outcome = "won"
+        full_message = "You played rock. They played " + computer_move + ". You " + outcome + "!"
+        render({ :plain =>  full_message })
+    end
+
+    def play_paper
+      moves = ["rock", "paper", "scissors"]
+      computer_move = moves.sample
+      if computer_move == "paper"
+        outcome = "tied"
+      elsif computer_move == "scissors"
+        outcome = "lost"
+      elsif computer_move == "rock"
+        outcome = "won"
+        full_message = "You played rock. They played " + computer_move + ". You " + outcome + "!"
+          render({ :plain =>  full_message })
+    end
+
+    def play_scissors
+      moves = ["rock", "paper", "scissors"]
+      computer_move = moves.sample
+      if computer_move == "scissors"
+        outcome = "tied"
+      elsif computer_move == "rock"
+        outcome = "lost"
+      elsif computer_move == "paper"
+        outcome = "won"
+
+      full_message = "You played rock. They played " + computer_move + ". You " + outcome + "!"
+      render({ :plain =>  full_message })
+    end
+       
+
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-  
+  get("/", { :controller => "application", :action => "homepage" })
+  get("/rock", { :controller => "application", :action => "play_rock" })
+  get("/paper", { :controller => "application", :action => "play_paper" })
+  get("/scissors", { :controller => "application", :action => "play_scissors" })
 
   # Write your routes above this line
   # =================================
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 end
