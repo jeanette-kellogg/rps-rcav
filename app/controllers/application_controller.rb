072fc8a8725c95ae54b64e74303f8efbe19aab79
jeanettestock <jeanettemstock@gmail.com>
2020-05-07 22:16:10 +0000

    Final Changes Added homepage, fixed paper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
-
-    def homepage
-      render({ :plain => "Hello, world!" })
-    end
     
+  def homepage
+        render({ :template => "game_templates/homepage.html.erb" })  
+      end
+
+
     def play_rock
       moves = ["rock", "paper", "scissors"]
       @comp_move = moves.sample
@@ -24,11 +25,11 @@ class ApplicationController < ActionController::Base
       moves = ["rock", "paper", "scissors"]
       @comp_move = moves.sample
 
-        if @comp_move == "rock"
+        if @comp_move == "paper"
           @outcome = "tied"
-        elsif @comp_move == "paper"
-          @outcome = "lost"
         elsif @comp_move == "scissors"
+          @outcome = "lost"
+        elsif @comp_move == "rock"
           @outcome = "won"
         end
         render({ :template => "game_templates/user_paper.html.erb" })  
@@ -38,11 +39,11 @@ class ApplicationController < ActionController::Base
       moves = ["rock", "paper", "scissors"]
        @comp_move = moves.sample
 
-        if @comp_move == "rock"
+        if @comp_move == "scissors"
           @outcome = "tied"
-        elsif @comp_move == "paper"
+        elsif @comp_move == "rock"
           @outcome = "lost"
-        elsif @comp_move == "scissors"
+        elsif @comp_move == "paper"
           @outcome = "won"
         end
         render({ :template => "game_templates/user_scissors.html.erb" })  
